d832c3a20663be59bf750c4640dbae7a90567f6b
biddls <biddls@users.noreply.github.com>
2020-04-05 18:41:56 +0100

    stuff

--- getdat/cleanGSI.py
+++ getdat/cleanGSI.py
@@ -1,61 +1,75 @@
 import time
 
 
-def main(data):
+def clean(data):
     player = data.get('player')
-    if player.get('observer_slot') == 1:
-        map = data.get('map')
+    map = data.get('map')
+    round = data.get('round')
 
-        round = data.get('round')
+    try:
+        round.pop('win_team')
+    except:
+        pass
 
-        try:
-            round.pop('win_team')
-        except:
-            pass
-
-        if len(round) < 2:
-            round = [round.get('phase'), 'not planted']
-
-        else:
-            round = [round.get('phase'), round.get('bomb')]
-
-        map = [map.get('team_ct').get('score'), map.get('team_t').get('score'), round[0], round[1]]
-
-        stats = player.get('match_stats')
-        stats = [stats.get('kills'), stats.get('assists'), stats.get('deaths'), stats.get('mvps'), stats.get('score')]
-
-        state = player.get('state')
-        if state.get('round_killshs') == None:
-            HSs = 0
-        else:
-            HSs = state.get('round_killshs')
-        state = [state.get('health'), state.get('flashed'), state.get('smoked'), state.get('burning'),
-                 state.get('round_kills'), HSs]
-
-        player = [player.get('team')]
-
-        player += state + stats
-
-        data = map + player
-
-        path = 'C:\\Users\\thoma\\OneDrive\\Documents\\PycharmProjects\\CS GO AI 2\\data\\game data\\data.txt'
-        dat = open(path, 'a+')
-        string = str(data[0])
-
-        for x in data[1:]:
-            string += ', ' + str(x)
-
-        dat.write(str(time.time()) + string + '\r')
-
-        dat.close()
-
-        key = ['ct rounds', 't rounds', 'round phase', 'bomb phase', 'players team', 'health', 'flashed', 'smoked',
-               'burning', 'round kills', 'round kills hs', 'kills', 'assists', 'deaths', 'mvps', 'score'] #and time idk why i have that may remove
-
-        prnt = []
-        for x in range(len(key)):
-            prnt.append([key[x], data[x]])
-
-        print(prnt)
+    if len(round) < 2:
+        round = [round.get('phase'), 'not planted']
     else:
-        print('Oberserving/ dead')
+        round = [round.get('phase'), round.get('bomb')]
+
+    map = [map.get('team_ct').get('score'), map.get('team_t').get('score'), round[0], round[1]]
+
+    stats = player.get('match_stats')
+    stats = [stats.get('kills'), stats.get('assists'), stats.get('deaths'), stats.get('mvps'), stats.get('score')]
+
+    state = player.get('state')
+    if state.get('round_killshs') == None:
+        HSs = 0
+    else:
+        HSs = state.get('round_killshs')
+    state = [state.get('health'), state.get('flashed'), state.get('smoked'), state.get('burning'),
+             state.get('round_kills'), HSs]
+
+    player = [player.get('team')]
+
+    player += state + stats
+
+    data = map + player
+    path = 'C:\\Users\\thoma\\OneDrive\\Documents\\PycharmProjects\\CS GO AI 2\\data\\game data\\data.txt'
+    dat = open(path, 'a+')
+    string = str(data[0])
+
+    for x in data:
+        string += ', ' + str(x)
+
+    dat.write(str(time.time()) + string + '\r')
+
+    dat.close()
+
+    key = ['ct rounds', 't rounds', 'round phase', 'bomb phase', 'players team', 'health', 'flashed', 'smoked',
+           'burning', 'round kills', 'round kills hs', 'kills', 'assists', 'deaths', 'mvps',
+           'score']  # and time idk why i have that may remove
+
+    prnt = []
+    for x in range(len(key)):
+        prnt.append([key[x], data[x]])
+
+    # print(prnt)
+
+
+def main(data):
+    try:
+        test = data.get('player').get('state').get('health')
+        if data.get('player').get('state').get('health') > 0:
+            clean(data)
+
+        else:
+            path = 'C:\\Users\\thoma\\OneDrive\\Documents\\PycharmProjects\\CS GO AI 2\\data\\game data\\data.txt'
+            dat = open(path, 'a+')
+
+            clean(data)
+            dat.write('DIED\r')
+
+            dat.close()
+    except:
+        open('C:\\Users\\thoma\\OneDrive\\Documents\\PycharmProjects\\CS GO AI 2\\data\\game data\\data.txt',
+             "w+").close()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import threading
 import time
+from time import sleep
 from getdat import getalldat
 from getdat import ctrls
+import numpy as np
 
 ###plan###
 # get image in
@@ -25,20 +27,87 @@ class myThread(threading.Thread):
 
     def run(self):
         print("Starting " + self.name)
-        self.file = getupdate(self.path)
+        while True:
+            try:
+                self.file = self.parse(open(self.path))
+            except:
+                pass
+
+    def parse(self, file):
+        data = file.read().split('\n')
+
+        for line in range(len(data)):
+            data[line] = data[line].split(', ')
+            for index in range(len(data[line])):
+                try:
+                    data[line][index] = float(data[line][index])
+                except:
+                    pass
+
+        data = np.array(data)
+        if len(data) > 1:
+            return data[:-1]
+        else:
+            return None
+
+class GSI(threading.Thread):
+    def __init__(self, threadID, name, ):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.previous = None
+        self.current = None
+        self.score = 0
+        self.deaths = 0
+
+    def run(self):
+        print("Starting " + self.name)
+
+        while True:
+            sleep(1)
+            if self.previous is None:
+                self.previous = self.current
+            elif self.previous[-1][0] != self.current[-1][0]:
+                self.previous = self.reduce()
+
+    def reduce(self):
+        print('hi')
+        new = self.current
+        old = self.previous[0]
+        print(new,'&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        for index, x in enumerate(new):
+            print(index, x)
+            if x == old:
+                new = new[:-index]
+
+        print(new, '###################################')
+
+        """swapped = 1
+        while swapped > 0:
+            swapped = 0
+            for n1, n2 in zip(reversed(array[1:]), reversed(array[:-1])):
+                if n1 == n2:
+                    swapped += 1
+                    #remove element should result in array being only filled w uniqe entrys where in each entry something that is being measured gets changed"""
 
 
-def getupdate(path):
-    try:
-        file = open(path)
-        return file
-    except:
-        print('doesnt exist')
+
+key = ['time', 'ct rounds', 't rounds', 'round phase', 'bomb phase', 'players team', 'health', 'flashed', 'smoked', 'burning', 'round kills', 'round kills hs', 'kills', 'assists', 'deaths', 'mvps', 'score']
 
 
-if __name__ == '__main__':
+def setup():
+    open('C:\\Users\\thoma\\OneDrive\\Documents\\PycharmProjects\\CS GO AI 2\\data\\game data\\data.txt', "w+").close()
     getalldat.GSIstart()
-    thread1 = myThread(1, "Thread-1")
+    get_data = myThread(1, 'get data')
+    csinfo = GSI(2, 'GSI')
 
     # Start new Threads
-    thread1.start()
+    get_data.start()
+    csinfo.start()
+
+    while True:
+        sleep(3)
+        csinfo.current = get_data.file
+
+if __name__ == '__main__':
+    setup()
